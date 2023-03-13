b4d4bd2a0ceddf9394523d9440a08b3dc9a11e3d
Albert Hudayberdin <arh1744@yandex.ru>
2023-03-13 19:14:39 +0500

    Initial commit spring-data-jpa

---
+++ src/main/java/ru/bera/springboot/spring_data_jpa/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package ru.bera.springboot.spring_data_jpa.service;
+
+
+import ru.bera.springboot.spring_data_jpa.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+
+    public List<Employee> findAllByName(String name);
+}
